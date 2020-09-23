cdb3d79b425f0fafda56088d3036e5e2cb9d1cf4
PetrR <agronom20005@gmail.com>
2020-09-23 20:18:45 +0300

    add fix

--- backend/views/layouts/sidebar.php
+++ backend/views/layouts/sidebar.php
@@ -10,10 +10,6 @@ use common\models\User;
 ?>
 
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <?php
-    var_dump(Yii::$app->user->identity->role_id);
-    var_dump(Yii::$app->user->identity->roleOption->access_pages);
-    ?>
     <!-- Brand Logo -->
     <a href="<?=\yii\helpers\Url::home()?>" class="brand-link">
         <img src="<?=$assetDir?>/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
@@ -23,51 +19,12 @@ use common\models\User;
 
     <!-- Sidebar -->
     <div class="sidebar">
-        <!-- Sidebar user panel (optional) -->
-<!--        <div class="user-panel mt-3 pb-3 mb-3 d-flex">-->
-<!--            <div class="image">-->
-<!--                <img src="--><?//=$assetDir?><!--/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">-->
-<!--            </div>-->
-<!--            <div class="info">-->
-<!--                <a href="#" class="d-block">Администратор</a>-->
-<!--            </div>-->
-<!--        </div>-->
-
         <!-- Sidebar Menu -->
         <nav class="mt-2">
 
             <?php
             echo \hail812\adminlte3\widgets\Menu::widget([
                 'items' => [
-//                    [
-//                        'label' => 'Контент сайта',
-//                        'icon' => 'tachometer-alt',
-//                        //'badge' => '<span class="right badge badge-info">2</span>',
-//                        'items' => [
-//                            ['label' => 'Страницы', 'url' => ['page/index'], 'iconStyle' => 'far'],
-//                            ['label' => 'Новости', 'url' => ['article/index'], 'iconStyle' => 'far'],
-//                        ]
-//                    ],
-//                    [
-//                        'label' => 'Пользователи',
-//                        'icon' => 'tachometer-alt',
-//                        //'badge' => '<span class="right badge badge-info">2</span>',
-//                        'items' => [
-//                            ['label' => 'Страницы', 'url' => ['site/index'], 'iconStyle' => 'far'],
-//                            ['label' => 'Новости', 'iconStyle' => 'far'],
-//                        ]
-//                    ],
-//                    [
-//                        'label' => 'Абоненты',
-//                        'icon' => 'tachometer-alt',
-//                        //'badge' => '<span class="right badge badge-info">2</span>',
-//                        'items' => [
-//                            ['label' => 'Страницы', 'url' => ['site/index'], 'iconStyle' => 'far'],
-//                            ['label' => 'Новости', 'iconStyle' => 'far'],
-//                        ]
-//                    ],
-//
-//                    ['label' => 'Simple Link', 'icon' => 'th', 'badge' => '<span class="right badge badge-danger">New</span>'],
                     //['label' => 'Yii2 PROVIDED', 'header' => true],
                     ['label' => 'Login', 'url' => ['site/login'], 'icon' => 'sign-in-alt', 'visible' => Yii::$app->user->isGuest],
                     //['label' => 'Gii',  'icon' => 'file-code', 'url' => ['/gii'], 'target' => '_blank'],
@@ -77,15 +34,15 @@ use common\models\User;
                     ['label' => 'Новости', 'url' => ['article/index'], 'iconStyle' => 'far', 'visible' => Yii::$app->user->identity->roleOption->access_news == 1],
                     ['label' => 'Управление сайтом', 'header' => true, 'visible' => Yii::$app->user->identity->roleOption->access_users == 1],
                     [
-                        'label' => 'Пользователи',
+                        'label' => 'Администраторы','iconStyle' => 'far', 'iconClassAdded' => 'text-danger',
                         'items' => [
-                            ['label' => 'Список пользователей', 'url' => ['user/index'], 'iconStyle' => 'far', 'visible' => Yii::$app->user->identity->roleOption->access_users == 1],
+                            ['label' => 'Список админов', 'url' => ['user/index'], 'iconStyle' => 'far', 'visible' => Yii::$app->user->identity->roleOption->access_users == 1],
                             ['label' => 'Права доступа', 'url' => ['roles/index'], 'iconStyle' => 'far', 'visible' => Yii::$app->user->identity->roleOption->access_users == 1]
                         ]
                     ],
-                    ['label' => 'Управление абенентами', 'header' => true, 'visible' => Yii::$app->user->identity->roleOption->access_abonents == 1],
+                    //['label' => 'Управление абонентами', 'header' => true, 'visible' => Yii::$app->user->identity->roleOption->access_abonents == 1],
                     [
-                        'label' => 'Абоненты',
+                        'label' => 'Абоненты','iconClass' => 'nav-icon far fa-circle text-warning',
                         'items' => [
                             ['label' => 'Список абенентов', 'iconStyle' => 'far', 'visible' => Yii::$app->user->identity->roleOption->access_abonents == 1],
                             ['label' => 'еще что-то с абонентами', 'iconStyle' => 'far', 'visible' => Yii::$app->user->identity->roleOption->access_abonents == 1]
