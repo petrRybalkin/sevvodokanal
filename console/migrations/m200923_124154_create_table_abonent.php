b7b0de6ef8c7167c54f5875b8e36e93097452d46
PetrR <agronom20005@gmail.com>
2020-09-24 12:34:06 +0300

    test migration

---
+++ console/migrations/m200923_124154_create_table_abonent.php
@@ -0,0 +1,54 @@
+<?php
+
+//namespace yii\queue\db\migrations;
+
+use yii\db\Migration;
+
+/**
+ * Class m200923_124154_create_table_abonent
+ */
+class m200923_124154_create_table_abonent extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        $this->createTable('{{%client}}', [
+            'id' => $this->primaryKey(),
+            'username' => $this->string()->notNull()->unique(),
+            'auth_key' => $this->string(32)->notNull(),
+            'password_hash' => $this->string()->notNull(),
+            'password_reset_token' => $this->string()->unique(),
+            'phone' => $this->string()->notNull()->unique(),
+            'email' => $this->string()->notNull()->unique(),
+            'status' => $this->smallInteger()->notNull()->defaultValue(10),
+            'created_at' => $this->integer()->notNull(),
+            'updated_at' => $this->integer()->notNull(),
+        ], $tableOptions);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%client}}');
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "M200923160959AddAbonentTable cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
---
+++ console/migrations/m200923_181328_add_verification_token.php
@@ -0,0 +1,42 @@
+<?php
+
+//namespace yii\queue\db\migrations;
+
+use yii\db\Migration;
+
+/**
+ * Class m200923_181328_add_verification_token
+ */
+class m200923_181328_add_verification_token extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->addColumn('{{%client}}', 'verification_token', $this->string()->defaultValue(null));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropColumn('{{%client}}', 'verification_token');
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "M200923181328AddVerificationToken cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
