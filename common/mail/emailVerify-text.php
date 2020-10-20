6c009d3670b242676c1bff0ce0ad8fd346464701
PetrR <agronom20005@gmail.com>
2020-10-20 18:36:24 +0300

    fix translate email templates

--- common/mail/emailVerify-text.php
+++ common/mail/emailVerify-text.php
@@ -5,8 +5,8 @@
 
 $verifyLink = Yii::$app->urlManager->createAbsoluteUrl(['site/verify-email', 'token' => $user->verification_token]);
 ?>
-Hello <?= $user->username ?>,
+Доброго часу доби <?= $user->username ?>,
 
-Follow the link below to verify your email:
+Перейдіть за посиланням нижче, щоб підтвердити свою електронну адресу:
 
 <?= $verifyLink ?>
