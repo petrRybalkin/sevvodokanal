e10d0122d00b16244ccda007a8c044a36a0da38a
Petr Rybalkin <agronom20005@gmail.com>
2020-12-22 13:30:11 +0200

    add payment button

--- frontend/views/profile/payment.php
+++ frontend/views/profile/payment.php
@@ -15,5 +15,14 @@ $this->params['breadcrumbs'][] = $this->title;
 </div>
 
 
+<div class="mt-6">
+    <a href="https://next.privat24.ua/payments/form/{'token' : 'c43a7e21620e00a0a4697f5364f07eaf4jfdmqbf', 'personalAccount':'0908405420290', 'sum':'1.00'}">
+    <button type="submit" name="add-meter-button" class="group relative flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-green-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out">
+        Оплатить
+    </button>
+    </a>
+</div>
+
+
 
 
