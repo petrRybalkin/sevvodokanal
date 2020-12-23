c3dd8bd89815a4f9f2a4a58c4d002bc7f5322992
Petr Rybalkin <agronom20005@gmail.com>
2020-12-23 10:11:58 +0200

    fix

--- frontend/views/profile/payment.php
+++ frontend/views/profile/payment.php
@@ -22,7 +22,7 @@ if ($debt > 0) {
 <div class="mt-6">
     <a href='https://next.privat24.ua/payments/form/{"token" : "c43a7e21620e00a0a4697f5364f07eaf4jfdmqbf", "personalAccount":"<?=$indication->account_number?>", "sum":"<?=$sum?>"}'>
     <button type="submit" name="add-meter-button" class="group relative flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-green-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out">
-        Оплатить
+        Оплатити
     </button>
     </a>
 </div>
