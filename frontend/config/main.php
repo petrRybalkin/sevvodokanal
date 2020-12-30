2710cbda4d3ad9852c4567aa9e80ea3c0c8d0b4d
Petr Rybalkin <agronom20005@gmail.com>
2020-12-30 18:25:46 +0200

    fix-deleteBootstrapAssets

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -15,6 +15,19 @@ return [
     'bootstrap' => ['log'],
     'controllerNamespace' => 'frontend\controllers',
     'components' => [
+        'assetManager' => [
+            'bundles' => [
+//                'yii\web\JqueryAsset' => [
+//                    'js'=>[]
+//                ],
+//                'yii\bootstrap\BootstrapPluginAsset' => [
+//                    'js'=>[]
+//                ],
+                'yii\bootstrap\BootstrapAsset' => [
+                    'css' => [],
+                ],
+            ],
+        ],
         'request' => [
             'csrfParam' => '_csrf-frontend',
         ],
